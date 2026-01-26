759548e1b393c513ca01bd640818b6f5425182de
Chetna Saini <chetnasaini108@gmail.com>
2026-01-26 15:59:11 +0530

    Assignment 1

---
+++ assignment 1.py
@@ -0,0 +1,11 @@
+num1 =int(input('enter the first number:'))
+num2 = int(input("enter the second number :"))
+add = num1 + num2
+print('Addition:',add)
+sub = num1 - num2
+print('Subtraction:',sub)
+mul = num1* num2
+print('Multiplication:', mul)
+div = num1/num2
+print ('Division:' , div)
+
