d30442089e8f217c524697fbfafa7ecad7bb77f9
chetnasaini108-cyber <chetnasaini108@gmail.com>
2026-02-25 19:01:54 +0530

    Add files via upload

--- assignment2.py
+++ assignment2.py
@@ -2,11 +2,13 @@
 number = int(input('Enter a number:'))
 if number % 2 == 0:
      print(f'The number {number} is even.')
- else:
+else:
      print(f'The number {number} is odd.')
 
 # TASK2
-for i in range(1,51,1):
-    print(i)
-total = sum(range(1,51))
-print(f'The sum of numbers from 1 to 50 is : {total}.')
+n=0
+for i in range(51):
+    n+=i
+    print(n)
+
+print(f'The sum of numbers from 1 to 50 is : {n}.')
